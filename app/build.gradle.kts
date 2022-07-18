67f37f0ad5aef6a6b74b387357acacd8460d9258
Тюльканов Михаил Вячеславович <MTyulkanov@alfabank.ru>
2022-07-18 17:10:37 +0400

    Init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use JCenter for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("klox.AppKt")
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions {
+        jvmTarget = "18"
+    }
+}
---
+++ app/src/main/kotlin/klox/App.kt
@@ -0,0 +1,12 @@
+package klox
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello World!"
+        }
+}
+
+fun main() {
+    println(App().greeting)
+}
