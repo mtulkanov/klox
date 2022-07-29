374b73a091018e90ffa9f7815f05925bcd68c32c
Тюльканов Михаил Вячеславович <MTyulkanov@alfabank.ru>
2022-07-29 22:52:57 +0400

    Read source code from file or command prompt

--- app/src/main/kotlin/klox/App.kt
+++ app/src/main/kotlin/klox/App.kt
@@ -1,12 +1,33 @@
 package klox
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
+import java.io.File
+import java.nio.charset.StandardCharsets
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size > 1) {
+        println("Usage: jlox [script]")
+        exitProcess(64)
+    } else if (args.size == 1) {
+         runFile(args[0])
+    } else {
+         runPrompt()
+    }
 }
 
-fun main() {
-    println(App().greeting)
+fun runPrompt() {
+    while (true) {
+        print("> ")
+        val sourceCode = readLine() ?: break
+        runSourceCode(sourceCode)
+    }
+}
+
+fun runFile(filename: String) {
+    val sourceCode = File(filename).readText(StandardCharsets.UTF_8)
+    runSourceCode(sourceCode)
+}
+
+fun runSourceCode(sourceCode: String) {
+    TODO("Not yet implemented")
 }
